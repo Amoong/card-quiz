b28dd585bbb2d57f9e043273e5d70506bbc9173e
Amoong <moo1323@naver.com>
2023-06-03 09:17:35 +0900

    Add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "/card-quiz/",
   resolve: {
     alias: {
       src: path.resolve("src/"),
